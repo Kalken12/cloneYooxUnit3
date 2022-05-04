26cc61ef6684b4800f696195ba74681d15d9452f
Kalyani Kenekar <82832370+Kalken12@users.noreply.github.com>
2022-05-04 19:15:35 +0530

    This is first commit
    making clone Yoox.com in second
    construct week

---
+++ data/data.js
@@ -0,0 +1,144 @@
+let corousal1 = [
+
+    {
+        id:1,
+        image: "https://www.yoox.com/images/items/17/17215863RC_14_f.jpg?impolicy=crop&width=306&height=390",
+        name: "MMS MAISON MARGIELA",
+        type: "Casual Pants",
+        strike: "US$ 432,00 38%OFF",
+        price: "259"
+    },
+    {
+        id:2,
+        image: "https://www.yoox.com/images/items/14/14204982GM_14_f.jpg?impolicy=crop&width=306&height=390",
+        name: "MSGM",
+        type: "Sweater",
+        strike: "US$ 432,00 38%OFF",
+        price: "300"
+    },
+    {
+        id:3,
+        image: "https://www.yoox.com/images/items/46/46802203FN_14_f.jpg?impolicy=crop&width=306&height=390",
+        name: "Leather",
+        type: "Wallet",
+        strike: "US$ 132,00 38%OFF",
+        price: "200"
+    },
+    {
+
+id:4,
+        image: "https://www.yoox.com/images/items/13/13740160WD_14_f.jpg?impolicy=crop&width=306&height=390",
+        name: "CHRISTOPHER KANE",
+        type: "Midi Skirt",
+        strike: "US$ 432,00 38%OFF",
+        price: "259"
+    },
+    {
+
+        id:5,
+        image: "https://www.yoox.com/images/items/15/15192946UD_14_f.jpg?impolicy=crop&width=306&height=390",
+        name: "LISA MARIE FERNANDIZ",
+        type: "long dress",
+        strike: "US$ 432,00 38%OFF",
+        price:  "500"
+    },
+    {
+
+        id:6,
+        image: "https://www.yoox.com/images/items/15/15192942OB_14_f.jpg?impolicy=crop&width=306&height=390",
+        name: "LISA MARIE FERNANDIZ",
+        type: "long dress",
+        strike: "US$ 432,00 38%OFF",
+        price: "400"
+    },
+    {
+id:7,
+        image: "https://www.yoox.com/images/items/17/17206290SX_14_f.jpg?impolicy=crop&width=306&height=390",
+        name: "PARIS TEXAS",
+        type: "Boots",
+        strike: "US$ 432,00 38%OFF",
+        price: "1000"
+    },
+
+]
+let corousal2 = [
+
+    {
+        Id: 1,
+        image: "https://www.yoox.com/images/items/13/13774231AS_14_f.jpg?impolicy=crop&width=306&height=390",
+        name: "TIBI",
+        type: "modest fashion",
+        strike: "US$ 432,00 60%OFF",
+        price: "163,00"
+    },
+    {
+        id: 2,
+        image: "https://www.yoox.com/images/items/13/13741344LQ_14_f.jpg?impolicy=crop&width=306&height=390",
+        name: "THEORY",
+        type: "Casual Pants",
+        strike: "US$ 432,00 38%OFF",
+        price: "259,00"
+    },
+    {
+        id: 3,
+        image: "https://www.yoox.com/images/items/12/12785574XM_14_f.jpg?impolicy=crop&width=306&height=390",
+        name: "Caseu",
+        type: "Sweatshirt",
+        strike: "US$ 432,00 38%OFF",
+        price: "359"
+    },
+    {
+        id: 4,
+        image: "https://www.yoox.com/images/items/50/50272153XN_14_f.jpg?impolicy=crop&width=306&height=390",
+        name: "silver",
+        type: "Breslet",
+        strike: "US$ 432,00 38%OFF",
+        price: "100"
+    },
+    {
+        id: 5,
+        image: "https://www.yoox.com/images/items/16/16116715NQ_14_f.jpg?impolicy=crop&width=306&height=390",
+        name: "LORO PIANE",
+        type: "Full length Jacket",
+        strike: "US$ 432,00 38%OFF",
+        price: "400"
+    },
+    {
+        id: 6,
+        image: "https://www.yoox.com/images/items/45/45659883KR_14_f.jpg?impolicy=crop&width=306&height=390",
+        name: "KARL LERD",
+        type: "Cross body bag",
+        strike: "US$ 432,00 38%OFF",
+        price: "200"
+    },
+
+
+]
+
+let data3=[
+    {
+        id: 1,
+        image: "https://www.yoox.com/images/items/45/45585284SD_14_f.jpg?impolicy=crop&width=306&height=390",
+        name: "DOLSE GOLDENA",
+        type: "Hand Bag",
+        strike: "US$ 432,00 38%OFF",
+        price: "350"
+    },
+    {
+        id:2,
+        image: "https://www.yoox.com/images/items/14/14204982GM_14_f.jpg?impolicy=crop&width=306&height=390",
+        name: "MSGM",
+        type: "Sweater",
+        strike: "US$ 432,00 38%OFF",
+        price: "300"
+    },
+    {
+        id: 3,
+        image: "https://www.yoox.com/images/items/12/12785574XM_14_f.jpg?impolicy=crop&width=306&height=390",
+        name: "Caseu",
+        type: "Sweatshirt",
+        strike: "US$ 432,00 38%OFF",
+        price: "359"
+    },
+   
+]
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { navbar, footer } from "../components/navbar.js"
+
+
+
+
+
+document.getElementById("navbar").innerHTML = navbar()
+
+ document.getElementById("footer").innerHTML = footer()
+//  document.getElementById("footerBottom").innerHTML =  
+
+
+
+
+
+    
