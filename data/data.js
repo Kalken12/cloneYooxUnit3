66b0b727ea5cebbe838b82306404f432a0f708b5
Kalyani Kenekar <82832370+Kalken12@users.noreply.github.com>
2022-05-06 00:54:24 +0530

    made payment and OTP page  with functionality
     made payment and OTP page  with functionality

--- data/data.js
+++ data/data.js
@@ -1,7 +1,7 @@
 let corousal1 = [
 
     {
-        id:1,
+        id: 1,
         image: "https://www.yoox.com/images/items/17/17215863RC_14_f.jpg?impolicy=crop&width=306&height=390",
         name: "MMS MAISON MARGIELA",
         type: "Casual Pants",
@@ -9,7 +9,7 @@ let corousal1 = [
         price: "259"
     },
     {
-        id:2,
+        id: 2,
         image: "https://www.yoox.com/images/items/14/14204982GM_14_f.jpg?impolicy=crop&width=306&height=390",
         name: "MSGM",
         type: "Sweater",
@@ -17,7 +17,7 @@ let corousal1 = [
         price: "300"
     },
     {
-        id:3,
+        id: 3,
         image: "https://www.yoox.com/images/items/46/46802203FN_14_f.jpg?impolicy=crop&width=306&height=390",
         name: "Leather",
         type: "Wallet",
@@ -26,7 +26,7 @@ let corousal1 = [
     },
     {
 
-id:4,
+        id: 4,
         image: "https://www.yoox.com/images/items/13/13740160WD_14_f.jpg?impolicy=crop&width=306&height=390",
         name: "CHRISTOPHER KANE",
         type: "Midi Skirt",
@@ -35,16 +35,16 @@ id:4,
     },
     {
 
-        id:5,
+        id: 5,
         image: "https://www.yoox.com/images/items/15/15192946UD_14_f.jpg?impolicy=crop&width=306&height=390",
         name: "LISA MARIE FERNANDIZ",
         type: "long dress",
         strike: "US$ 432,00 38%OFF",
-        price:  "500"
+        price: "500"
     },
     {
 
-        id:6,
+        id: 6,
         image: "https://www.yoox.com/images/items/15/15192942OB_14_f.jpg?impolicy=crop&width=306&height=390",
         name: "LISA MARIE FERNANDIZ",
         type: "long dress",
@@ -52,7 +52,7 @@ id:4,
         price: "400"
     },
     {
-id:7,
+        id: 7,
         image: "https://www.yoox.com/images/items/17/17206290SX_14_f.jpg?impolicy=crop&width=306&height=390",
         name: "PARIS TEXAS",
         type: "Boots",
@@ -115,7 +115,7 @@ let corousal2 = [
 
 ]
 
-let data3=[
+let data3 = [
     {
         id: 1,
         image: "https://www.yoox.com/images/items/45/45585284SD_14_f.jpg?impolicy=crop&width=306&height=390",
@@ -125,7 +125,7 @@ let data3=[
         price: "350"
     },
     {
-        id:2,
+        id: 2,
         image: "https://www.yoox.com/images/items/14/14204982GM_14_f.jpg?impolicy=crop&width=306&height=390",
         name: "MSGM",
         type: "Sweater",
@@ -140,5 +140,458 @@ let data3=[
         strike: "US$ 432,00 38%OFF",
         price: "359"
     },
-   
+
 ]
+
+// data for women shopping page
+var info1 = [
+    {
+        image_url:
+            "https://images.bewakoof.com/t320/not-them-boyfriend-t-shirt-463718-1641538268-1.jpg",
+        name: "Not them Boyfriend T-shirts",
+        category:"T-SHIRT",
+        brand:"LEV'S",
+        price: 1999,
+        strikedoffprice: 3199,
+    },
+    {
+        image_url:
+            "https://images.bewakoof.com/t320/so-normal-is-boring-garfield-boyfriend-t-shirt-459643-1640954419-1.jpg",
+        name: "so normal is boring t-shirt",
+        category:"T-SHIRT",
+        brand: "SAINT LAURENT",
+        price: 769,
+        strikedoffprice: 1099,
+    },
+    {
+        image_url:
+            "https://images.bewakoof.com/t320/orchid-petal-women-s-hoodie-sweatshirt-454660-1640787479-1.jpg",
+        name: "feel good lilac hoodie",
+        category:"T-SHIRT",
+        brand:"LEV'S",
+        price: 1299,
+        strikedoffprice: 1440,
+    },
+    {
+        image_url:
+            "https://images.bewakoof.com/t320/orchid-petal-aop-half-sleeves-t-shirt-455845-1640787455-1.jpg",
+        name: "feel good lilac half T-shirts",
+
+        category:"T-SHIRT",
+        brand: "SAINT LAURENT",
+        price: 699,
+        strikedoffprice: 999,
+    },
+    {
+        image_url:
+            "https://images.bewakoof.com/t320/know-your-power-boyfriend-t-shirt-464873-1642404499-1.jpg",
+        name: "Know your power boyfriend T-shirts",
+        category:"T-SHIRT",
+        brand: "SAINT LAURENT",
+        price: 1620,
+        strikedoffprice: 3599,
+    },
+    {
+        image_url:
+            "https://images.bewakoof.com/t320/lemon-squeeze-boyfriend-t-shirt-464699-1642075280-1.jpg",
+        name: "lemon Squeeze boyfriend T-shirts",
+        category:"T-SHIRT",
+        brand:"LEV'S",
+        price: 1620,
+        strikedoffprice: 3599,
+    },
+    {
+        image_url:
+            "https://images.bewakoof.com/t320/orchid-petal-wax-yellow-chest-panel-t-shirt-454734-1640787333-1.jpg",
+        name: "Feel good lilac panel T_shirts",
+        category:"T-SHIRT",
+        brand: "SAINT LAURENT",
+        price: 599,
+        strikedoffprice: 999,
+    },
+    {
+        image_url:
+            "https://images.bewakoof.com/t320/orchid-petal-nimbus-cloud-color-block-full-sleeve-sweatshirt-454791-1640787092-1.jpg",
+        name: "feel good color block T_Shirt",
+        category:"T-SHIRT",
+        price: 1575,
+        strikedoffprice: 3499,
+    },
+
+    {
+        image_url:
+            "https://images.bewakoof.com/t320/hanging-mickey-half-sleeve-t-shirt-463714-1641537084-1.jpg",
+        name: "hanging Micky Half sleeve T-shirt",
+        brand: "SAINT LAURENT",
+        category:"T-SHIRT",
+        price: 1999,
+        strikedoffprice: 3199,
+    },
+    {
+        image_url:
+            "https://images.bewakoof.com/t320/weekend-mood-tom-tjl-women-s-half-sleeve-printed-t-shirt-354953-1621409959.jpg",
+        name: "Weekend mood Tom half sleeve T_shirt",
+        category:"T-SHIRT",
+        brand:"LEV'S",
+        price: 769,
+        strikedoffprice: 1099,
+    },
+    {
+        image_url:
+            "https://images.bewakoof.com/t320/lemon-squeeze-half-sleeve-t-shirt-464702-1642075241-1.jpg",
+        name: "Lemon Sqeeze half Sleeve T-shirt",
+        category:"T-SHIRT",
+        brand: "CALVIN KLEIN",
+        price: 1299,
+        strikedoffprice: 1440,
+    },
+    {
+        image_url:
+            "https://images.bewakoof.com/t320/women-s-printed-boyfriend-fit-christmas-t-shirt-443521-1640008934-1.jpg",
+        name: "Women typography printed T-shirt",
+        brand: "LEV'S",
+        category:"T-SHIRT",
+        price: 699,
+        strikedoffprice: 999,
+    },
+    {
+        image_url:
+            "https://images.bewakoof.com/t320/women-s-printed-boyfriend-fit-christmas-t-shirt-443522-1640008891-1.jpg",
+        name: "marron boyfriend Fit printed T-shirt",
+        brand: "SAINT LAURENT",
+        category:"T-SHIRT",
+        price: 1620,
+        strikedoffprice: 3599,
+    },
+    {
+        image_url:
+            "https://images.bewakoof.com/t320/never-lose-hope-boyfriend-t-shirt-463273-1641456986-1.jpg",
+        name: "Never loose hope Boyfriend T-shirt",
+        brand: "CALVIN KLEIN",
+        category:"T-SHIRT",
+        price: 1620,
+        strikedoffprice: 3599,
+    },
+    {
+        image_url:
+            "https://images.bewakoof.com/t320/not-them-half-sleeve-t-shirt-463727-1641538285-1.jpg",
+        name: "Not them half sleeve T-shirt",
+        brand: "LEV'S",
+        price: 599,
+        category: "T-SHIRT",
+        strikedoffprice: 999,
+    },
+    {
+        image_url:
+            "https://images.bewakoof.com/t320/black-plus-size-solid-hoodie-aw-21-362774-1640101816-1.jpg",
+        name: "Black plus size solid hoodie",
+        brand: "LEV'S",
+        category: "HOODIE",
+        price: 1575,
+        strikedoffprice: 3499,
+    },
+    {
+        image_url:
+            "https://images.bewakoof.com/t320/killer-brown-full-sleeve-t-shirt-394634-1634895235-1.jpg",
+        name: "Killer brown full sleeve T-shirt",
+
+        brand: "SAINT LAURENT",
+
+        category: "T-SHIRT",
+        price: 1620,
+        strikedoffprice: 3599,
+    },
+    {
+        image_url:
+            "https://images.bewakoof.com/t320/white-all-over-printed-boyfriend-t-shirt-381168-1640701612-1.JPG",
+        name: "white all over printed boyfriend t-shirt",
+        brand: "CALVIN KLEIN",
+        category: "T-SHIRT",
+        price: 1620,
+        strikedoffprice: 3599,
+    },
+    {
+        image_url:
+            "https://images.bewakoof.com/t320/women-s-drop-shoulder-pink-solid-hoodie-368271-1637134535-1.jpg",
+        name: "women drop shoulder pink solid hoodie",
+        brand: "LEV'S",
+        category: "HOODIE",
+        price: 599,
+        strikedoffprice: 999,
+    },
+    {
+        image_url:
+            "https://images.bewakoof.com/t320/sage-susan-fashion-sweatshirt-hoodie-dress-aw-21-368342-1638532121-1.jpg",
+        name: "women solid sage green hoodie Dress",
+        brand: "CALVIN KLEIN",
+        category: "HOODIE",
+        price: 1575,
+        strikedoffprice: 3499,
+    },
+
+];
+//   Data for womens page
+var mensData = [
+    {
+        image_url:
+            "https://th.bing.com/th/id/OIP.mQfXzaUWdVDXAChf-aKLZwHaJ4?w=196&h=261&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+        name: "MEN'S CALVIN KLEIN TRACK JACKET",
+        brand: "CALVIN KLEIN",
+        category: "JACKET",
+        Symbol: "₹",
+        price: 750,
+        strikedoffprice: 2530,
+    },
+    {
+        image_url:
+            "https://www.yoox.com/images/items/41/41992286qo_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name: "MEN'S HERON PRESTON JACKET ",
+        brand: "LEV'S",
+        category: "JACKET",
+        price: 450,
+        strikedoffprice: 999,
+        Symbol: "₹",
+    },
+    {
+        image_url:
+            "https://www.yoox.com/images/items/13/13539975ds_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name: " Wales Bonner Power geometric-print trousers",
+        brand: "CALVIN KLEIN",
+        category: "PANT",
+        price: 801,
+        strikedoffprice: 1099,
+        Symbol: "₹",
+    },
+    {
+        image_url:
+            "https://www.yoox.com/images/items/38/38983820ws_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name: "Alexander McQueen intarsia-pattern Shirt",
+        brand: "SAINT LAURENT",
+        category: "SHIRT",
+        price: 399,
+        strikedoffprice: 799,
+        Symbol: "₹",
+    },
+    {
+        image_url:
+            "https://www.yoox.com/images/items/12/12611459fg_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name: "JW Anderson x Pol Anglada appliqué-logo levis shirt ",
+        brand: "LEV'S",
+        price: 1620,
+        strikedoffprice: 3599,
+        category: "SHIRT",
+        Symbol: "₹",
+    },
+    {
+        image_url:
+            "https://www.yoox.com/images/items/12/12621828bw_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name: "Calvin Klein herringbone-effect jacket ",
+        brand: "CALVIN KLEIN",
+        price: 1440,
+        strikedoffprice: 3199,
+        category: "JACKET",
+        Symbol: "₹",
+    },
+    {
+        image_url:
+            "https://www.yoox.com/images/items/38/38988984ku_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name: " MEN'S Gervon check-print overshirt ",
+        brand: "LEV'S",
+        price: 1999,
+        strikedoffprice: 3199,
+        category: "SHIRT",
+        Symbol: "₹",
+    },
+    {
+        image_url:
+            "https://www.yoox.com/images/items/12/12596941qq_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name: "MEN'S TRAINING T-SHIRTS TEE ",
+        brand: "SAINT LAURENT",
+        price: 769,
+        strikedoffprice: 1099,
+        category: "T-SHIRT",
+        Symbol: "₹",
+    },
+    {
+        image_url:
+            "https://www.yoox.com/images/items/13/13516570hu_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name: "Calvin Klein slim-fit pant ",
+        brand: "CALVIN KLEIN",
+        price: 1299,
+        strikedoffprice: 1440,
+        category: "PANT",
+        Symbol: "₹",
+    },
+    {
+        image_url:
+            "https://cdn-images.farfetch-contents.com/16/96/82/31/16968231_33877299_600.jpg ",
+        name: "Kenzo cropped tailored trousers",
+        brand: "KENZO",
+        price: 699,
+        strikedoffprice: 999,
+        category: "PANT",
+        Symbol: "₹",
+    },
+    {
+        image_url:
+            "https://www.yoox.com/images/items/13/13626258op_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name: "Kenzo Arrows intarsia-knit pant",
+        brand: "KENZO",
+        category: "PANT",
+        price: 1620,
+        strikedoffprice: 3599,
+        Symbol: "₹",
+    },
+    {
+        image_url:
+            "https://cdn-images.farfetch-contents.com/17/69/35/47/17693547_37152483_600.jpg ",
+        name: "Off-White low-top sneakers",
+        brand: "OFF WHITE",
+        category: "SHOES",
+        price: 1620,
+        strikedoffprice: 3599,
+        Symbol: "₹",
+    },
+    {
+        image_url:
+            "https://www.yoox.com/images/items/11/11943151iu_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name: "Saint Laurent lace-up suede Shoes ",
+        brand: "SAINT LAURENT",
+        category: "SHOES",
+        price: 3289,
+        strikedoffprice: 4578,
+        Symbol: "₹",
+    },
+    {
+        image_url:
+            "https://www.yoox.com/images/items/17/17180233bx_14_e.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name: " MEN'S MARNI RUNNING RUNNER SHOES",
+        brand: "MARNI",
+        category: "SHOES",
+        price: 1575,
+        strikedoffprice: 3499,
+        Symbol: "₹",
+    },
+    {
+        image_url:
+            "https://www.yoox.com/images/items/17/17158884gm_14_e.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name: "Off-White palm tree high-top sneakers ",
+        brand: "OFF WHITE",
+        category: "SHOES",
+        price: 3799,
+        strikedoffprice: 4500,
+        Symbol: "₹",
+    },
+    {
+        image_url:
+            "https://cdn-images.farfetch-contents.com/17/38/87/94/17388794_36294381_600.jpg",
+        name: " Off-White Arrows leather-trim backpack",
+        brand: "OFF WHITE",
+        category: "BAG",
+        price: 779,
+        strikedoffprice: 1299,
+        Symbol: "₹",
+    },
+    {
+        image_url:
+            "https://www.yoox.com/images/items/13/13620501db_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name: "MEN'S MARNI TRAINING FOUNDATION  PANTS  ",
+        brand: "MARNI",
+        category: "PANT",
+        price: 1019,
+        strikedoffprice: 1699,
+        Symbol: "₹",
+    },
+    {
+        image_url:
+            "https://www.yoox.com/images/items/13/13468861xa_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name: "MEN'S KENZO TRAINING FOUNDATION PANTS  ",
+        brand: "KENZO",
+        category: "PANT",
+        price: 1031,
+        strikedoffprice: 1699,
+        Symbol: "₹",
+    },
+    {
+        image_url:
+            "https://cdn-images.farfetch-contents.com/16/86/53/38/16865338_36146527_600.jpg",
+        name: "Marni slogan-print tote bag ",
+        brand: "MARNI",
+        category: "BAG",
+        price: 1595,
+        strikedoffprice: 3399,
+        Symbol: "₹",
+    },
+    {
+        image_url:
+            "https://www.yoox.com/images/items/17/17130113ab_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name: "  MEN'S KENZO RUNNING ZTAUR RUN SHOES ",
+        brand: "KENZO",
+        category: "SHOES",
+        price: 3749,
+        strikedoffprice: 4989,
+        Symbol: "₹",
+    },
+    {
+        image_url:
+            " https://cdn-images.farfetch-contents.com/16/32/31/49/16323149_37142236_600.jpg ",
+        name: "Paris EMERALDS DAYTONA",
+        brand: "KENZO",
+        category: "WATCH",
+        price: 767,
+        strikedoffprice: 1499,
+        Symbol: "₹",
+    },
+    {
+        image_url:
+            "https://cdn-images.farfetch-contents.com/17/72/28/77/17722877_37116982_600.jpg",
+        name: "Paris Nobile quartz 43mm",
+        brand: "KENZO",
+        category: "WATCH",
+        price: 1159,
+        strikedoffprice: 2299,
+        Symbol: "₹",
+    },
+    {
+        image_url:
+            "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/alphafly-outsole-1623266757.jpg?crop=0.660xw:0.440xh;0.216xw,0.144xh&resize=480:*",
+        name: " MEN'S NIKE RUNNING GENESIS RUNNER SHOE  ",
+        brand: "NIKE",
+        category: "SHOES",
+        price: 1620,
+        strikedoffprice: 3599,
+        Symbol: "₹",
+    },
+    {
+        image_url:
+            "https://www.yoox.com/images/items/12/12577488fb_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name: "UNISEX NIKE CLASSICS T-SHIRT",
+        brand: "NIKE",
+        category: "T-SHIRT",
+        price: 3910,
+        strikedoffprice: 5580,
+        Symbol: "₹",
+    },
+    {
+        image_url:
+            "https://cdn-images.farfetch-contents.com/17/29/77/93/17297793_36378668_600.jpg",
+        name: " Gucci Eyewear aviator-frame sunglasses ",
+        brand: "GUCCI",
+        category: "SUNGLASSES",
+        price: 2895,
+        strikedoffprice: 4268,
+        Symbol: "₹",
+    },
+    {
+        image_url:
+            "https://cdn-images.farfetch-contents.com/17/29/93/01/17299301_36471919_600.jpg",
+        name: "Gucci Eyewear aviator-frame sunglasses",
+        brand: "GUCCI",
+        category: "SUNGLASSES",
+        price: 3350,
+        strikedoffprice: 4799,
+        Symbol: "₹",
+    },
+
+
+];
